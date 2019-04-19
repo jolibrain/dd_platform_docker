ebcf2919448d5ac9fda6af76fd689db5ec022ebf
Alexandre Girard <git@alexgirard.com>
2019-04-19 15:33:26 +0200

    fix: in update.sh, Verify that DD_PLATFORM exists, and set it if not

--- code/cpu/update.sh
+++ code/cpu/update.sh
@@ -2,6 +2,9 @@
 export CURRENT_UID=$(id -u):$(id -g)
 export MUID=$(id -u)
 
+# Verify that DD_PLATFORM exists, and set it if not
+if [ -z ${DD_PLATFORM} ]; then export DD_PLATFORM=$PWD/../..; fi
+
 # Update platform configuration
 git pull
 
