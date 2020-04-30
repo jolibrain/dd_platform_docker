f386a86ab809af5f993b1f62172d12fe962e9d9f
Alexandre Girard <git@alexgirard.com>
2020-04-30 16:35:52 +0200

    fix: refactor update.sh script to include project_name parameter
    project name parameter will now be used in docker-compose command in
    order to setup dd_platform docker installation.
    
    update.sh scripts have been updated in order to check the presence of
    this mandatory parameter.
    
    It'll also check if a docker container associated with this project name
    is already running on the docker environment.

--- code/gpu/update.sh
+++ code/gpu/update.sh
@@ -1,4 +1,81 @@
-#!/bin/sh
+#!/bin/bash
+
+script="update.sh"
+#Declare the number of mandatory args
+margs=1
+
+# Common functions - BEGIN
+function example {
+    echo -e "example: $script -p PROJECT_NAME"
+}
+
+function usage {
+    echo -e "usage: $script MANDATORY [OPTION]\n"
+}
+
+function help {
+  usage
+    echo -e "MANDATORY:"
+    echo -e "  -p, --project-name PROJECT_NAME Project name used for this docker-compose installation"
+    echo -e "OPTION:"
+    echo -e "  -h, --help                      Prints this help\n"
+  example
+}
+
+# Ensures that the number of passed args are at least equals
+# to the declared number of mandatory args.
+# It also handles the special case of the -h or --help arg.
+function margs_precheck {
+    if [ $2 ] && [ $1 -lt $margs ]; then
+        if [ $2 == "--help" ] || [ $2 == "-h" ]; then
+            help
+            exit
+        else
+            usage
+            example
+            exit 1 # error
+        fi
+    fi
+}
+
+# Ensures that all the mandatory args are not empty
+function margs_check {
+    if [ $# -lt $margs ]; then
+        usage
+        example
+        exit 1 # error
+    fi
+}
+# Common functions - END
+
+# Main
+margs_precheck $# $1
+
+project_name=
+
+# Args while-loop
+while [ "$1" != "" ];
+do
+    case $1 in
+        -p  | --project-name )  shift
+                               project_name=$1
+                               ;;
+        -h   | --help )        help
+                               exit
+                               ;;
+        *)
+            echo "$script: illegal option $1"
+            usage
+            example
+            exit 1 # error
+            ;;
+    esac
+    shift
+done
+
+# Pass here your mandatory args for check
+margs_check $project_name
+
 export CURRENT_UID=$(id -u):$(id -g)
 export MUID=$(id -u)
 
@@ -16,11 +93,19 @@ docker-compose stop platform_ui nginx ouroboros
 docker-compose rm -v -f platform_ui nginx
 
 # Remove named volume
-docker volume rm -f gpu_platform_ui
+docker_platform_name=${project_name}_platform_ui
+if [[  $(docker ps --filter "name=^/$docker_platform_name$" --format '{{.Names}}') == $docker_platform_name ]];
+then
+    docker volume rm -f ${project_name}_platform_ui ;
+else
+    echo -e "WARNING: ${project_name}_platform_ui docker container is not running."
+    echo -e "Please verify the project name ${project_name} parameter and the current running containers."
+    echo -e "Command to check running docker container:"
+    echo -e "docker ps --format '{{.Names}}'\n"
+fi
 
 # Update containers
-docker-compose pull --no-parallel
+docker-compose pull --no-parallel nginx platform_ui
 
 # Restart with updated containers
-# Warning: this may kill your running jobs
-docker-compose up -d
+docker-compose up -d -p $project_name nginx platform_ui
