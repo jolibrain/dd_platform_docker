a934695206ab0f020d62a7dcea582a36d66c19da
Mehdi Abaakouk <sileht@sileht.net>
2020-09-18 08:46:18 +0200

    fix: remove useless rewrite api/dozzle rewriting rule
    Closes #29

--- ci/run.sh
+++ ci/run.sh
@@ -22,4 +22,6 @@ PORT=$(docker port "${PROJECT}_platform_ui_1" 80/tcp | awk -F: '{print $2}')
 timeout 22 sh -c "until nc -z localhost $PORT; do sleep 1; done"
 
 curl -s --head --request GET localhost:$PORT | head -1 | grep 'HTTP/1.1 200'
+curl -s --head --request GET http://localhost:$PORT/api/deepdetect/info | head -1 | grep 'HTTP/1.1 200'
 curl -s --head --request GET http://localhost:$PORT/filebrowser/sw.js | head -1 | grep 'HTTP/1.1 200'
+curl -s --head --request GET http://localhost:$PORT/docker-logs/ | head -1 | grep 'HTTP/1.1 200'
