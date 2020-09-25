d7633f9c9fe1c4f6c44699020d87410dd062b56e
Mehdi Abaakouk <sileht@sileht.net>
2020-09-25 11:33:43 +0200

    feat(tests): add more debug information

--- ci/run.sh
+++ ci/run.sh
@@ -7,19 +7,28 @@ set -o pipefail
 export MODE=$1
 export DD_PLATFORM=$PWD
 export DD_PORT=50000-50100
+export CURRENT_UID=$(id -u):$(id -g)
+export MUID=$(id -u)
+
 PROJECT=jenkins-test-${RANDOM}
+
 cd code/$MODE/
 
 cleanup() {
-    CURRENT_UID=$(id -u):$(id -g) MUID=$(id -u) docker-compose -p $PROJECT down --remove-orphans
+    ret=$?
+    docker-compose -p $PROJECT ps
+    docker-compose -p $PROJECT down --remove-orphans
+    exit=$ret
 }
 trap cleanup EXIT
 
-CURRENT_UID=$(id -u):$(id -g) MUID=$(id -u) docker-compose -p $PROJECT up -d
+docker-compose -p $PROJECT up -d
 
 PORT=$(docker port "${PROJECT}_platform_ui_1" 80/tcp | awk -F: '{print $2}')
 
-timeout 22 sh -c "until nc -z localhost $PORT; do sleep 1; done"
+timeout 60 sh -c "until nc -z localhost $PORT; do sleep 1; done"
+
+sleep 5
 
 curl -s --head --request GET localhost:$PORT | head -1 | grep 'HTTP/1.1 200'
 curl -s --head --request GET http://localhost:$PORT/api/deepdetect/info | head -1 | grep 'HTTP/1.1 200'
